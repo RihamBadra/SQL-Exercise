fdce4ab377f627ecec6c254e4b2248e704a34dd6
RihamBadra <rihamHbadra@gmail.com>
2021-06-02 14:04:42 +0300

    Done1- Creating Table

--- answers.sh
+++ answers.sh
@@ -1,5 +1,4 @@
 ###BASIC QUERIES:
-
 1) SELECT Name FROM students;
 2) SELECT * FROM students WHERE Age > 30;
 3) SELECT * FROM students WHERE Age > 30 AND Gender = "F";
@@ -10,3 +9,23 @@ VALUES ("Riham", 21, "F", 999);
 7) UPDATE students SET points=(points-10) WHERE name="Alex";
 
 ### Creating Table
+CREATE TABLE "graduates" (
+"ID" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+"Name" TEXT NOT Null UNIQUE,
+"Age" INTEGER,
+"Gender" TEXT,
+"Points" INTEGER,
+"Graduation" TEXT
+);
+
+INSERT INTO  graduates (Id,Name,Age,Gender,Points)
+SELECT Id,Name,Age,Gender,Points FROM students
+WHERE name="Layal";
+
+UPDATE graduates
+SET Graduation = '08/09/2018'
+WHERE name="Layal";
+
+DELETE FROM students WHERE name = "Layal";
+
+
